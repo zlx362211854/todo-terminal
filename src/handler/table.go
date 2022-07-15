655fbe9782f7c02a5110a39ce61db956b02633da
Link_Zhao <Link_Zhao@epam.com>
2022-07-15 16:53:08 +0800

    modify table row render

--- src/handler/table.go
+++ src/handler/table.go
@@ -1,17 +1,34 @@
 package handler
 
 import (
+	"fmt"
 	"os"
 	"terminal/service/ctx"
 
+	"github.com/fatih/color"
 	"github.com/jedib0t/go-pretty/v6/table"
 )
 
+func statusTransformer(val interface{}) string {
+	str := fmt.Sprintf("%v", val)
+	if val == "DONE" {
+		return color.GreenString(str)
+	}
+	return color.YellowString(str)
+}
 func ShowTodoTable(ctx *ctx.Ctx) {
 	data := ctx.Data.GetData()
+
 	t := table.NewWriter()
+	t.SetColumnConfigs([]table.ColumnConfig{
+		{
+			Name:        "Status",
+			Transformer: statusTransformer,
+		},
+	})
 	t.SetOutputMirror(os.Stdout)
 	t.AppendHeader(table.Row{"#", "Title", "Description", "Status"})
+
 	var rows []table.Row
 	for index, row := range data {
 		r := table.Row{
@@ -23,5 +40,4 @@ func ShowTodoTable(ctx *ctx.Ctx) {
 	t.AppendSeparator()
 	t.SetStyle(table.StyleColoredBright)
 	t.Render()
-
 }
