14dc314c204b2459801a480ec486586427265b05
Link_Zhao <Link_Zhao@epam.com>
2022-07-18 09:16:21 +0800

    modify pass through args if have any blank , add quotes above

--- bin/index.js
+++ bin/index.js
@@ -2,9 +2,16 @@
 var child_process = require("child_process");
 const os = require('os');
 var path = require('path')
-
 var args = process.argv.slice(2);
-var argStr = ' ' + args.join(' ')
+
+var argsWithQuotes = args.map(function (a) {
+    if (/\ /.test(a)) {
+        return `"${a}"`
+    }
+    return a
+})
+
+var argStr = ' ' + argsWithQuotes.join(' ')
 if (os.type() == 'Windows_NT') {
     //windows
     child_process.execSync(path.resolve(__dirname + '/main.exe') + argStr, { stdio: 'inherit' })
